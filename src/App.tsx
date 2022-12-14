3e4eadda809a17b98db521c62af9d014b02b6ccf
Jin Lee <jinslee@gmail.com>
2022-12-14 14:12:44 -0800

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App (): JSX.Element {
+  const theme = createTheme()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        lkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asdlkja sdkla sdk asd
+      </Grid>
+    </ThemeProvider>
   )
 }
 
