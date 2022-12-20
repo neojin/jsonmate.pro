d6c1b663e54b80a130a443424b4a3b4166baef9d
Jin Lee <jinslee@gmail.com>
2022-12-20 06:32:53 -0800

    add isvalidjson

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App(): JSX.Element {
   const theme = createTheme({
     palette: {
-      mode: 'dark',
+      mode: 'light',
     },
   });
+
+  const headerStyles = {
+    fontFamily: 'Roboto Mono',
+    fontSize: '1.5rem',
+    marginTop: '10px',
+  };
+
   const [json, setJson] = React.useState('');
   const [error, setError] = React.useState('');
+  const [isJsonValid, setIsJsonValid] = React.useState(false);
 
   const onBlur = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     let json = '';
@@ -22,41 +31,46 @@ function App(): JSX.Element {
       const parsedJson = JSON.parse(event.target.value);
       json = JSON.stringify(parsedJson, null, 2);
       setError('');
+      setIsJsonValid(true);
     } catch (e) {
       if (e instanceof SyntaxError) {
         setError(e.message);
         json = event.target.value;
+        setIsJsonValid(false);
       }
     }
     setJson(json);
   };
 
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setError('');
+    setIsJsonValid(false);
     setJson(event.target.value);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box>JSONMate</Box>
-      {error && json && (
-        <Box>
-          <Alert severity="error">{error}</Alert>
+      <Container maxWidth="xl">
+        <Box sx={headerStyles}>{'{jsonmate.pro}'}</Box>
+        <Box sx={{ paddingTop: '10px', paddingBottom: '10px' }}>
+          {error && !isJsonValid && <Alert severity="error">{error}</Alert>}
+          {!error && isJsonValid && <Alert severity="success">JSON is valid</Alert>}
         </Box>
-      )}
-      <Grid container component="main">
-        <form style={{ width: '100%' }}>
-          <TextField
-            style={{ width: '100%', fontSize: '1rem' }}
-            multiline
-            minRows={20}
-            placeholder="Paste your JSON here. Click anywhere else to format it."
-            onBlur={onBlur}
-            onChange={onChange}
-            value={json}
-          />
-        </form>
-      </Grid>
+        <Grid container component="main">
+          <form style={{ width: '100%' }}>
+            <TextField
+              style={{ width: '100%', fontSize: '1rem' }}
+              multiline
+              minRows={20}
+              placeholder="Paste your JSON here. Press tab or click anywhere else to format it."
+              onBlur={onBlur}
+              onChange={onChange}
+              value={json}
+            />
+          </form>
+        </Grid>
+      </Container>
     </ThemeProvider>
   );
 }
