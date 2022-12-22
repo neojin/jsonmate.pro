66f4562301659a585fe140020437b92915274cf2
Jin Lee <jinslee@gmail.com>
2022-12-21 16:26:23 -0800

    add dynamic height

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
@@ -6,26 +6,75 @@ import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import useWindowDimensions from './hooks/useWindowDimensions';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
 
 function App(): JSX.Element {
   const theme = createTheme({
     palette: {
-      mode: 'light',
+      background: {
+        default: '#eeeeee',
+      },
     },
   });
 
-  const headerStyles = {
-    fontFamily: 'Roboto Mono',
-    fontSize: '1.5rem',
-    marginTop: '10px',
+  const styles = {
+    header: {
+      fontFamily: 'Roboto Mono',
+      fontSize: '1.5rem',
+      marginTop: '10px',
+    },
+    alert: {
+      paddingTop: '10px',
+      paddingBottom: '10px',
+    },
+    form: {
+      width: '100%',
+    },
+    jsonTextarea: {
+      width: '100%',
+      fontSize: '1rem',
+    },
   };
 
-  const [json, setJson] = React.useState('');
-  const [error, setError] = React.useState('');
-  const [isJsonValid, setIsJsonValid] = React.useState(false);
+  const [json, setJson] = useState('');
+  const [error, setError] = useState('');
+  const [isJsonValid, setIsJsonValid] = useState(false);
+  const { height } = useWindowDimensions();
+  const [textareaRows, setTextareaRows] = useState(0);
+
+  useEffect(() => {
+    const nHeight = height - 200;
+    if (nHeight >= 1000) {
+      setTextareaRows(Math.floor(height / 30));
+    } else if (nHeight >= 800) {
+      setTextareaRows(Math.floor(height / 33));
+    } else if (nHeight >= 600) {
+      setTextareaRows(Math.floor(height / 40));
+    } else if (nHeight >= 400) {
+      setTextareaRows(Math.floor(height / 48));
+    } else {
+      setTextareaRows(Math.floor(height / 60) || 1);
+    }
+  }, [height]);
 
   const onBlur = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     let json = '';
+
+    if (!event.target.value) {
+      setJson('');
+      setError('');
+      setIsJsonValid(false);
+      return;
+    }
 
     try {
       const parsedJson = JSON.parse(event.target.value);
@@ -50,27 +99,52 @@ function App(): JSX.Element {
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth="xl">
-        <Box sx={headerStyles}>{'{jsonmate.pro}'}</Box>
-        <Box sx={{ paddingTop: '10px', paddingBottom: '10px' }}>
-          {error && !isJsonValid && <Alert severity="error">{error}</Alert>}
-          {!error && isJsonValid && <Alert severity="success">JSON is valid</Alert>}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+        }}
+      >
+        <CssBaseline />
+        <Container maxWidth="xl">
+          <Box sx={styles.header}>{'jsonmate.pro'}</Box>
+          <Box sx={styles.alert}>
+            {error && !isJsonValid && <Alert severity="error">{error}</Alert>}
+            {!error && isJsonValid && <Alert severity="success">JSON is valid</Alert>}
+          </Box>
+          <Grid container component="main">
+            <form style={styles.form}>
+              <TextField
+                style={styles.jsonTextarea}
+                multiline
+                minRows={textareaRows}
+                maxRows={textareaRows}
+                placeholder="Paste your JSON here. Press tab or click anywhere else to format it."
+                onBlur={onBlur}
+                onChange={onChange}
+                value={json}
+              />
+            </form>
+          </Grid>
+        </Container>
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1">My sticky footer can be found here.</Typography>
+          </Container>
         </Box>
-        <Grid container component="main">
-          <form style={{ width: '100%' }}>
-            <TextField
-              style={{ width: '100%', fontSize: '1rem' }}
-              multiline
-              minRows={20}
-              placeholder="Paste your JSON here. Press tab or click anywhere else to format it."
-              onBlur={onBlur}
-              onChange={onChange}
-              value={json}
-            />
-          </form>
-        </Grid>
-      </Container>
+      </Box>
     </ThemeProvider>
   );
 }
